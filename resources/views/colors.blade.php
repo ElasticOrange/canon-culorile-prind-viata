87e2efebeac6af893a36dcf2221a728195bdb4ba
mihaibarbu <xwave21@gmail.com>
2015-04-08 15:21:34 +0300

    Add overflow slider

--- resources/views/colors.blade.php
+++ resources/views/colors.blade.php
@@ -10,19 +10,23 @@
 			</div>
 		</div>
 	</div>
+	<div class="colorata" id="ascuns">
+		<img src="/img/colorata.png" />
+	</div>
 </div>
 
 <script>
 $('#slider2').slider({
-	range: "min",
-	min: 1,
-	max: 99,
-	value: 0,
+	range: true,
+	min: 0,
+	max: 100,
+	value: 1,
     slide: handleSliderChange
 });
 
 function handleSliderChange(event, slider){
           $(".sageata").css('left', slider.value + '%');
+          $("#ascuns").css('width', slider.value/100*465+235 + 'px');
 }
 </script>
 @endsection
