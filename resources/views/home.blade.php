2a51f7ed788ff0aaf9657c36436580a3b57fe67e
mihaibarbu <xwave21@gmail.com>
2015-04-09 21:33:43 +0300

    Add changes to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,8 @@
 @section('content')
 <div class="hometop">
 	<!-- <img src="/img/homeheader.png" /> -->
-	<div class="cerc2">
-		<img src="/img/cerc-colors.png" />
-		<h3 class="cerc-text">Cartusele originale au culori intense</h3>
-		<a href="/inscriere">
-			<h3 class="text-buton-cerc">Mai departe</h3>
-			<img class= "buton-cerc" src="/img/homebuton.png" />
-		</a>
+	<div class="cerc-home">
+		<img src="/img/bulina_pag1.png" />
 	</div>
 	<a href="/colors">
 		<img src="/img/homebuton.png" class="homebuton" />
