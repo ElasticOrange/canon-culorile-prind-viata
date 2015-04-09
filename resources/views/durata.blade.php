7be87bb1f9dd5c6121c1e8e6f7d6bb7085f6bd1e
mihaibarbu <xwave21@gmail.com>
2015-04-09 18:22:58 +0300

    Add durata page

--- resources/views/durata.blade.php
+++ resources/views/durata.blade.php
@@ -1,7 +1,22 @@
 @extends('app')
 
 @section('content')
+<div class="durata">
+	<div class="slider">
+			<div class="sageata">
+				<img src="/img/sageata-slider2.jpg" />
+			</div>
+	</div>
 
+	<div class="cerc-durata">
+		<img src="/img/cerc-colors.png" />
+		<h3 class="cerc-text">Cartusele originale au culori intense</h3>
+		<a href="#">
+			<h3 class="text-buton-cerc">Mai departe</h3>
+			<img class= "buton-cerc" src="/img/homebuton.png" />
+		</a>
+	</div>
+</div>
 
 <script>
 $('.slider').slider({
@@ -14,12 +29,17 @@ $('.slider').slider({
 
 function handleSliderChange(event, slider){
           $(".sageata").css('left', slider.value + '%');
-          $(".ascuns2").css('opacity', slider.value/100+0.10);
+          $(".sageata").text(slider.value*3+30 + 'ani');
+          // $(".ascuns2").css('opacity', slider.value/100+0.10);
 
-          if ($(".ascuns2").css('opacity') >= '0.95') 
+          if ($(".sageata").css('left') >= '85%') 
           	{
-          		return $(".cerc-sharp").show();
-          	};
+          		return $(".cerc-durata").show();
+          	}
+          	// else
+          	// {
+          	// 	return $(".cerc-durata").hide();
+          	// };
 }
 </script>
 @endsection
